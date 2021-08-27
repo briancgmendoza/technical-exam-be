865cb5d0e178dd9f617238fa36462036ac8bc755
Brian Christopher Mendoza <78596685+briancgmendoza@users.noreply.github.com>
2021-08-28 01:08:32 +0800

    Add project files.

---
+++ TechnicalExam/Controllers/EmployeeController.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+// Importing System.Data.SqlClient
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using TechnicalExam.Models;
+
+namespace TechnicalExam.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // Get Request
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select EmployeeMasterID, LastName, FirstName, MiddleName, Age, Address, PhoneNumber 
+                            from dbo.EmployeeMaster";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        // Post Request
+        [HttpPost]
+        public JsonResult Post(Employee emp)
+        {
+            string query = @"
+                            insert into dbo.EmployeeMaster
+                            (LastName, FirstName, MiddleName, Age, Address, PhoneNumber)
+                    values (@LastName, @Firstname, @MiddleName, @Age, @Address, @PhoneNumber)
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@LastName", emp.LastName);
+                    myCommand.Parameters.AddWithValue("@FirstName", emp.FirstName);
+                    myCommand.Parameters.AddWithValue("@MiddleName", emp.MiddleName);
+                    myCommand.Parameters.AddWithValue("@Age", emp.Age);
+                    myCommand.Parameters.AddWithValue("@Address", emp.Address);
+                    myCommand.Parameters.AddWithValue("@PhoneNumber", emp.PhoneNumber);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+        // Update Request
+        [HttpPut]
+        public JsonResult Put(Employee emp)
+        {
+            string query = @"
+                           update dbo.EmployeeMaster
+                           set LastName = @LastName,
+                            FirstName = @FirstName,
+                            MiddleName = @MiddleName,
+                            Age = @Age,
+                            Address = @Address,
+                            PhoneNumber = @PhoneNumber
+                            where EmployeeMasterID = @EmployeeMasterID
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@EmployeeMasterID", emp.EmployeeMasterID);
+                    myCommand.Parameters.AddWithValue("@LastName", emp.LastName);
+                    myCommand.Parameters.AddWithValue("@FirstName", emp.FirstName);
+                    myCommand.Parameters.AddWithValue("@MiddleName", emp.MiddleName);
+                    myCommand.Parameters.AddWithValue("@Age", emp.Age);
+                    myCommand.Parameters.AddWithValue("@Address", emp.Address);
+                    myCommand.Parameters.AddWithValue("@PhoneNumber", emp.PhoneNumber);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        // Delete Request
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                           delete from dbo.EmployeeMaster
+                            where EmployeeMasterID = @EmployeeMasterID
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@EmployeeMasterID", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+    }
+}
---
+++ TechnicalExam/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+// Importing mvc.NewtonsoftJson
+using Newtonsoft.Json.Serialization;
+
+
+namespace TechnicalExam
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Enabling CORS
+            services.AddCors((x) => {
+                x.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+            // JSON Serializer
+            services.AddControllersWithViews().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+                .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = 
+                new DefaultContractResolver());
+
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Enabling CORS
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
